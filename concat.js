9da033996ef71f78d2908b45b643eb941f148833
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-03 21:16:46 +0100

    concat function used done

---
+++ concat.js
@@ -0,0 +1,14 @@
+/**Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log(). */
+const arrayOfStringOne = ['S','h','u'];
+
+const arrayOfStringTwo = ['v','o'];
+
+const arrayOfStringThree = arrayOfStringOne.concat(arrayOfStringTwo);
+console.log(arrayOfStringOne);
+console.log(arrayOfStringTwo);
+console.log(arrayOfStringThree);
